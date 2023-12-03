a441ff06b662d3e989df18d4a747552c7e58c15e
studTon <evertonsdasilva1@gmail.com>
2023-12-03 17:50:41 -0300

    Complete functionalities Everything is under golden ratio here! :D

--- app.py
+++ app.py
@@ -10,13 +10,31 @@ app = Flask(__name__)
 def index():
     '''Página inicial da aplicação.''' 
     return render_template("index.html")
+
 @app.route('/', methods=["POST"])
 def get_username():
-    '''Página de perfil que mostra as informações públicas do usuário.'''
+    '''Página de perfil que mostra as informações públicas do usuário.
+    As informações são processadas numa sequência para avaliar as estatísticas.'''
+
     user = request.form['user']
     resposta = requests.get(
         f'https://api.github.com/users/{user}',
         headers={'Accept': 'application/json'},
         timeout=10)
-    body = resposta.json()
-    return render_template("pass.html", dado=body)
+    body = json.loads(resposta.text)
+    nome = body['name']
+    conta = body['login']
+    biografia = body['bio']
+    local = body['location']
+    repositorios = body['public_repos']
+    n_seguidores = body['followers']
+    n_conexoes = body['following']
+
+    return render_template("pass.html", 
+                           name=nome,
+                           login=conta,
+                           bio=biografia,
+                           location=local,
+                           repos=repositorios,
+                           followers=n_seguidores,
+                           following=n_conexoes)
